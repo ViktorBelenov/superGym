14cc2f1c18a35c6d06b1edba7bb0d31639c9bb64
Vitya <82025167+ViktorBelenov@users.noreply.github.com>
2023-08-22 14:40:23 +0300

    validation

--- source/js/main.js
+++ source/js/main.js
@@ -2,6 +2,7 @@ import {iosVhFix} from './utils/ios-vh-fix';
 import {initTabs} from './init-tabs';
 import {initAccordions} from './init-accordion';
 import {initCoachesSwiper, initFeedbackSwiper} from './init-swiper';
+import {Form} from './form-validate/form';
 import './init-video';
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -11,4 +12,10 @@ window.addEventListener('DOMContentLoaded', () => {
   initCoachesSwiper();
   initFeedbackSwiper();
   initAccordions();
+
+  window.addEventListener('load', () => {
+    const form = new Form();
+    window.form = form;
+    form.init();
+  });
 });
